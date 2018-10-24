6dd018b8a20a0ebfb9357200758fcb9bba2aa39a
wandrianomenjanahary <43283429+wandrianomenjanahary@users.noreply.github.com>
2018-10-24 11:03:37 +0300

    Configuration mrp.production
    Ajout des champs supplémentaires de mrp.production

--- mim/mrp_mim/models/__init__.py
+++ mim/mrp_mim/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import mrp
+from . import mrp_bom, mrp_production, product_product
---
+++ mim/mrp_mim/models/mrp_production.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, exceptions, _
+
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    state = fields.Selection(
+        string="Statut",
+        selection=[
+            ('confirmed', 'Confirmed'),
+            ('verified', u'Fiche vérifiée'),
+            ('validated', u'Fiche de débit validée'),
+            ('picking_except', 'Picking Exception'),
+            ('planned', 'Planned'),
+            ('progress', 'In Progress'),
+            ('done', 'Done'),
+            ('cancel', 'Cancelled')])
+    """Informations générales"""
+    description = fields.Char(
+        string="Description")
+    partner_id = fields.Many2one(
+        string="Partenaire",
+        comodel_name="res.partner")
+    """Stockage"""
+    """Données en entrée fiche de débit"""
+    is_printable = fields.Boolean(
+        string="Fiche de débit standard")
+    is_calculated = fields.Boolean(
+        string="Fiche de débit calculée")
+    partner_name = fields.Char(
+        string="Nom du client",
+        default=_get_partner_name)
+    largeur = fields.Float(
+        string="Largeur")
+    hauteur = fields.Float(
+        string="Hauteur")
+    tms = fields.Float(
+        string="TMS")
+    style = fields.Selection(
+        string="Style",
+        selection=[
+            ('fr', u'A la française'),
+            ('en', 'A l\'anglaise')])
+    vitre = fields.Char(
+        string="Vitre")
+    remplissage_vitre = fields.Selection(
+        string="Remplissage de vitre",
+        selection=[
+            ('standard', 'Standard'),
+            ('pleine_2_3', '2/3 pleine'),
+            ('pleine_1_2', '1/2 pleine'),
+            ('pleine_1_3', '1/3 pleine'),
+            ('pleine_bardage', 'Pleine/bardage')])
+    intermediaire = fields.Selection(
+        string=u"Intermédiaire",
+        selection=[
+            ('sans', u'Sans intermédiaire'),
+            ('avec', u'Avec intermédiaire')])
+    type_intermediaire = fields.Selection(
+        string="Type d\'intermédiaire",
+        selection=[
+            ('vertical', 'Vertical'),
+            ('horizontal', 'Horizontal')])
+    moustiquaire = fields.Boolean(
+        string="Moustiquaire")
+    division = fields.Boolean(
+        string="Division")
+    nb_division = fields.Integer(
+        string="Nombre de division")
+    batis_id = fields.Char(
+        string=u"Bâtis")
+    longueur_barre = fields.Float(
+        string="Longueur barre")
