49ac3378c4b088b2fe945a0041790870bd280d5d
m0r7y <r.radoniaina11@gmail.com>
2019-02-22 11:57:01 +0000

    adding new modute gestion_paie by Alitiana

---
+++ mim/gestion_paie/__manifest__.py
@@ -0,0 +1,24 @@
+
+{
+	'name' : 'Paie Malagasy',
+	'category' : 'Human Ressources',
+	'version' : '3.0',
+	'sequence' : 1,
+	'description' : 'Gestion de paie Malagasy',
+	'website' : 'https://www.ingenosya.com',
+	'depends' : ['hr_contract','hr_payroll','base'],
+	'data' : [
+				'views/paie_view.xml',
+				'views/etat_salaire_view.xml',
+				'views/cnaps_view.xml',
+				'views/ostie_view.xml',
+				'views/irsa_view.xml',
+				'views/hr_payroll_view.xml',
+				# 'data/hr_payroll_data.xml',
+			],
+
+	'icon' : 'gestion_paie/static/src/img/icon.png',
+	'installable' : True,
+	'application' : True,
+	'auto_install' : False,
+}
---
+++ mim/gestion_paie/models/__init__.py
@@ -0,0 +1,6 @@
+from . import cnaps
+from . import ostie
+from . import etat_salaire
+from . import irsa
+from . import paie
+from . import hr_payroll
