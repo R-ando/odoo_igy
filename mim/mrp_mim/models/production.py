4a586abd5ce1827d40d1a6094ae36f2981e4c2c5
m0r7y <r.radoniaina11@gmail.com>
2019-02-22 11:12:50 +0000

    production.py > adding action_compute_stock_move

--- mim/mrp_mim/models/production.py
+++ mim/mrp_mim/models/production.py
@@ -210,6 +210,196 @@ class MrpProduction(models.Model):
         qty_barres = qty_mm / len_barre
         return ceil(qty_barres)
 
+        # Fontion calculant les données de product lines
+        @api.multi
+        def action_compute_stock_move(self):
+            results = []
+
+            for production in self:
+                res = self.prepare_lines(production)
+                results = res['lines_done']
+
+                # Calcul de la qualité des composants
+                # Récupération propriétés
+                qty = self.product_qty
+                largeur = self.largeur
+                hauteur = self.hauteur
+                tms = self.tms
+                localdict = {
+                    'largeur': largeur,
+                    'hauteur': hauteur,
+                    'tms': tms,
+                    'result': None,
+                    'style': self.style,
+                    'vitre': self.vitre,
+                }
+
+                # Mise è jour
+                if not self.vitre:
+                    localdict['type_vitre'] = 0
+                else:
+                    localdict['type_vitre'] = self.vitre.id
+                localdict['inter'] = self.intermediaire
+                localdict['moust'] = self.moustiquaire
+                localdict['div'] = self.division
+
+                if not self.nb_division:
+                    localdict['nb_div'] = 1.0
+                else:
+                    localdict['nb_div'] = self.nb_division
+
+                if not self.type_intermediaire or self.type_intermediaire == 'vert':
+                    localdict['type_inter'] = 'vert'
+                else:
+                    localdict['type_inter'] = 'horiz'
+
+                localdict['batis'] = self.batis_id.name
+
+                component = self.env['mrp.component']
+                list_components = {}
+
+                for line in results:
+                    line_id = line[1]['line_id']
+                    list_id = component.search([('line_id', '=', line_id)])
+                    if list_id:
+                        for c in component.browse(list_id).id:
+                            total1 = 0.0
+                            total2 = 0.0
+                            len_total0 = 0.0
+                            len_unit0 = 0.0
+                            qty_total0 = 0.0
+
+                            for s in c.sub_component_ids:
+                                localdict['Q'] = qty
+                                safe_eval(s.python_product_qty, localdict, mode='exec', nocopy=True)
+                                product_qty = float(localdict['result'])
+                                list_components['production_id'] = production.id
+                                list_components['product_qty'] = product_qty
+                                localdict['QU'] = list_components['product_qty']
+
+                                safe_eval(s.python_product_qty_total, localdict, mode='exec', nocopy=True)
+                                product_qty_total = float(localdict['result'])
+                                list_components['product_qty_total'] = product_qty_total
+
+                                qty_total0 = product_qty_total
+
+                                localdict['QT'] = list_components['product_qty_total']
+                                total2 = total2 + list_components['product_qty_total']
+
+                                if not line[1]['is_accessory']:
+                                    list_components['ref'] = c.product_parent_id.partner_ref
+                                    list_components['name'] = s.name
+
+                                    # Calcul de la longueur unitaire du composant
+                                    safe_eval(s.python_len_unit, localdict, mode='exec', nocopy=True)
+                                    len_unit = float(localdict['result'])
+                                    list_components['len_unit'] = len_unit
+                                    localdict['LU'] = list_components['len_unit']
+
+                                    # Calcul de la longueur totale du composant
+                                    safe_eval(s.python_len_total, localdict, mode='exec', nocopy=True)
+                                    len_total = float(localdict['result'])
+                                    list_components['len_total'] = len_total
+                                    # len_total0 = len_total
+
+                                    total1 = total1 + list_components['len_total']
+                                    LU = list_components['len_unit']
+                                    LT = list_components['len_total']
+
+                                    # if list_components['len_total'] != 0.0:
+                                    #     _logger.info("\n*****list_components = %s*****\n" % list_components)
+                                else:
+                                    if list_components['product_qty_total'] != 0.0:
+                                        list_components['ref'] = c.product_parent_id.name
+                                        list_components['name'] = s.name
+                                list_components = {}
+
+                            if not line[1]['is_accessory']:
+                                uom = line[0].product_uom_id.name
+                                ref = component.product_parent_id.partner_ref
+                                len_barre = self.longueur_barre
+
+                                if uom == 'Barre':
+                                    if ref != 'P50-MB':
+                                        line[1]['qty'] = self.get_nb_barres(total1)
+                                    else:
+                                        var = (LU / 100.0) * LT * qty_total0 / len_barre
+                                        line[1]['qty'] = self.round_float(var)
+                                else:
+                                    line[1]['qty'] = (LU * LT * product_qty_total) / 10000.0
+
+                            else:
+                                line[1]['qty'] = total2
+
+            return res['boms_done'], results
+
+        # Surcharge de la fonction _generate_moves
+        def _generate_moves(self):
+            for production in self:
+                production._generate_finished_moves()
+                # factor = production.product_uom_id._compute_quantity(production.product_qty,production.bom_id.product_uom_id) / production.bom_id.product_qty
+                boms, lines = production.action_compute_stock_move()  # Vérifier la valeur de retou
+                production._generate_raw_moves(lines)  # Création de stock.move avec la quantité calculer
+                # Check for all draft moves whether they are mto or not
+                production._adjust_procure_method()
+                production.move_raw_ids._action_confirm()
+            return True
+
+        # Surcharge de la fonction _generate_raw_moves
+        def _generate_raw_moves(self, exploded_lines):
+            self.ensure_one()
+            moves = self.env['stock.move']
+            for bom_line, line_data in exploded_lines:
+                move = self._generate_raw_move(bom_line, line_data)
+                if move != None:
+                    moves += move
+            return moves
+
+        # Surcharge de la fonction _generate_raw_move
+        def _generate_raw_move(self, bom_line, line_data):
+            quantity = line_data['qty']
+            # alt_op needed for the case when you explode phantom bom and all the lines will be consumed in the operation given by the parent bom line
+            alt_op = line_data['parent_line'] and line_data['parent_line'].operation_id.id or False
+            if bom_line.child_bom_id and bom_line.child_bom_id.type == 'phantom':
+                return self.env['stock.move']
+            if bom_line.product_id.type not in ['product', 'consu']:
+                return self.env['stock.move']
+            if self.routing_id:
+                routing = self.routing_id
+            else:
+                routing = self.bom_id.routing_id
+            if routing and routing.location_id:
+                source_location = routing.location_id
+            else:
+                source_location = self.location_src_id
+
+            if quantity != 0.0:
+                original_quantity = (self.product_qty - self.qty_produced) or 1.0
+                _logger.info("\n*****original_quantity = %s*****\n" % original_quantity)
+                data = {
+                    'sequence': bom_line.sequence,
+                    'name': self.name,
+                    'date': self.date_planned_start,
+                    'date_expected': self.date_planned_start,
+                    'bom_line_id': bom_line.id,
+                    'product_id': bom_line.product_id.id,
+                    'product_uom_qty': quantity,
+                    'product_uom': bom_line.product_uom_id.id,
+                    'location_id': source_location.id,
+                    'location_dest_id': self.product_id.property_stock_production.id,
+                    'raw_material_production_id': self.id,
+                    'company_id': self.company_id.id,
+                    'operation_id': bom_line.operation_id.id or alt_op,
+                    'price_unit': bom_line.product_id.standard_price,
+                    'procure_method': 'make_to_stock',
+                    'origin': self.name,
+                    'warehouse_id': source_location.get_warehouse().id,
+                    'group_id': self.procurement_group_id.id,
+                    'propagate': self.propagate,
+                    'unit_factor': quantity / original_quantity,
+                }
+                return self.env['stock.move'].create(data)
+
     # <last_commit>
 
     # new method
